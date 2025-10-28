dfbf764f53cecbe2a4172d3e6bb6c66e10868663
taufikmuharrom <taufikmuharromdev@gmail.com>
2025-10-28 20:38:13 +0700

    refactor(css): move global styles from App.vue to main.css

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,22 +8,4 @@
   </div>
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html, body, #app {
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-}
-
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+<style></style>
